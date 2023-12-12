4e201da659c60e8d172b7d14ad5f21a41c635003
Emmanuel Mora <emmanuelmora05@gmail.com>
2023-12-12 02:18:15 -0600

    Improve performance and made copyq required

--- scripts/clipboard.sh
+++ scripts/clipboard.sh
@@ -4,28 +4,12 @@ FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --header='Select clipboard history. Press TA
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$CURRENT_DIR/.envs"
 
-if ! [ -x "$(command -v copyq)" ]; then
-    action="buffer"
-elif [ -z "$1" ]; then
-    action="system"
-else
-    action="$1"
-fi
+_tmux_fzf_action_system() {
+  clipboard=$(copyq eval -- 'n=size(); for(i=0; i<n; i++) print("[" + str(i) + "] " + str(read(i)).split("\\").join("\\\\").split("\n").join("\\\\n")  + "\n")')
+  clipboard_index=$(printf "$clipboard" | eval "$TMUX_FZF_BIN $TMUX_FZF_OPTIONS --preview=\"printf {} | sed -E 's/^\[[0-9]+\] (.*)$/\1/'\"" | sed -E 's/^\[([0-9]+)\].*$/\1/')
+  [[ -z "$clipboard_index" ]] && exit -1
+  clipboard_contents=$(printf "$clipboard" | sed -n "$((clipboard_index+1))p" | sed -E 's/^\[[0-9]+\] (.*)$/\1/' | sed -E 's/\\\\/\\/g' | sed -E 's/\\n/\n/g' )
+  tmux set-buffer -b _temp_tmux_fzf "$clipboard_contents" && tmux paste-buffer -b _temp_tmux_fzf && tmux delete-buffer -b _temp_tmux_fzf
+}
 
-if [[ "$action" == "system" ]]; then
-    item_numbers=$(copyq count)
-    contents="[cancel]\n"
-    index=0
-    while [ "$index" -lt "$item_numbers" ]; do
-        _content="$(copyq read ${index} | tr '\n' ' ' | tr '\\n' ' ')"
-        contents="${contents}copy${index}: ${_content}\n"
-        index=$((index + 1))
-    done
-    copyq_index=$(printf "$contents" | eval "$TMUX_FZF_BIN $TMUX_FZF_OPTIONS --preview=\"echo {} | sed -e 's/^copy//' -e 's/: .*//' | xargs -I{} copyq read {}\"" | sed -e 's/^copy//' -e 's/: .*//')
-    [[ "$copyq_index" == "[cancel]" || -z "$copyq_index" ]] && exit
-    echo "$copyq_index" | xargs -I{} sh -c 'tmux set-buffer -b _temp_tmux_fzf "$(copyq read {})" && tmux paste-buffer -b _temp_tmux_fzf && tmux delete-buffer -b _temp_tmux_fzf'
-elif [[ "$action" == "buffer" ]]; then
-    selected_buffer=$(tmux list-buffers | sed -e 's/:.*bytes//' -e '1s/^/[cancel]\n/' -e 's/: "/: /' -e 's/"$//' | eval "$TMUX_FZF_BIN $TMUX_FZF_OPTIONS --preview=\"echo {} | sed -e 's/\[cancel\]//' -e 's/:.*$//' | head -1 | xargs tmux show-buffer -b\"" | sed 's/:.*$//')
-    [[ "$selected_buffer" == "[cancel]" || -z "$selected_buffer" ]] && exit
-    echo "$selected_buffer" | xargs -I{} sh -c 'tmux paste-buffer -b {}'
-fi
+_tmux_fzf_action_system
